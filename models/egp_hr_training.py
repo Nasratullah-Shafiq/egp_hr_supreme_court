9f64406066098c9998db1546d343a4a2c93e5ddb
Nasratullah-Shafiq <nasratullah.shafiq@gmail.com>
2025-12-26 11:39:20 +0430

    new changes

---
+++ models/egp_hr_training.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+from odoo import fields, models, api
+from odoo.exceptions import ValidationError
+
+class EgpHrInherit(models.Model):
+    _inherit = 'hr.employee'
+    _description = "Human Resource"
+
+    training_ids = fields.One2many('employee.training', 'employee_id', string='Training')
+
+# Your Python code (e.g., in a controller or model)
+
+class EmployeeTraining(models.Model):
+    _name = 'employee.training'
+    _description = 'Employee Training'
+
+    employee_id = fields.Many2one('hr.employee', string='Employee')
+
+    course_id = fields.Many2one('employee.course', string="Training Type", tracking=True)
+    training_location = fields.Char(string='Training Location')
+    training_start_date = fields.Date(string='Start Date')
+    training_end_date = fields.Date(string='End Date')
+    training_result = fields.Selection([('supreme', 'Supreme'), ('Excellent', 'Excellent'), ('Good', 'Good'),
+                                        ('medium', 'medium'), ('elementary', 'elementary')], string="Training Result")
+    training_certification = fields.Char(string='Certification')
+    training_remarks = fields.Text(string='Remarks')
+
+
+class EmployeeCourse(models.Model):
+    _name = 'employee.course'
+    _description = 'Employee Course'
+
+    name = fields.Char(string='Course')
+
+    @api.constrains('name')
+    def _check_unique_name(self):
+        for record in self:
+            if self.search_count([('name', '=', record.name)]) > 1:
+                raise ValidationError("The course name must be unique!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
